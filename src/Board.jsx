6ca0b55c0ae89ce171100363f8573a67ea6e0345
Elise Maschke <elise.maschke@outlook.de>
2019-07-20 12:35:34 +0200

    rearranged

--- src/Board.jsx
+++ src/Board.jsx
@@ -14,7 +14,7 @@ class Board extends Component {
 
   render() {
     return (
-      <div>
+      <div className="block">
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/Game.jsx
+++ src/Game.jsx
@@ -51,7 +51,7 @@ class Game extends Component {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button className="move-button" onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
     })
@@ -65,11 +65,12 @@ class Game extends Component {
 
     return (
       <div className="game">
+      <h1 className="block">Tic Tac Toe</h1>
         <div className="game-board">
           <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div className="status">{status}</div>
           <ul>{moves}</ul>
         </div>
       </div>
